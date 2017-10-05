4c421c4514ddd1b93454177f2d8d2e99c5f63296
mahajanl <lakshamima@gmail.com>
2017-10-05 15:10:43 -0400

    learning how to set up properties and fields

--- Week2Day4IntrotoOOP/Cat.cs
+++ Week2Day4IntrotoOOP/Cat.cs
@@ -8,5 +8,32 @@ namespace Week2Day4IntrotoOOP
 {
     class Cat
     {
+        //STATES
+        //types of STATES
+        //fields
+        private string name;
+
+        //Properties - kind of like a characteristic and kind of like a method
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //BEHAVIORS
+
+
+        //Constructors
+        public Cat()
+        {
+            // default constructors have nothing in parentheses
+
+        }
+
+
+
+
+
+
     }
 }
